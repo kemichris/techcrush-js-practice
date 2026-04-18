21814ab0820dcc6768a60723294077771e1074d3
kemi Chris <ekeminichristopher123@gmail.com>
2026-04-18 11:18:44 +0100

    question 3

---
+++ question3.js
@@ -0,0 +1,6 @@
+// Write a function called greetUser that takes a name parameter and returns the string: 'Welcome to TechCrush, [name]!'. Test it with your own name.
+function greetUser(userName) {
+    return 'Welcome to TechCrush, ' + userName + '!';
+}
+
+console.log(greetUser("Christopher"));
