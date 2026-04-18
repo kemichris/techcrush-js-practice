83801212fde9df3712eb7701cf713d75e759aa07
kemi Chris <ekeminichristopher123@gmail.com>
2026-04-18 11:13:51 +0100

    Initializing repo

---
+++ question1.js
@@ -0,0 +1,7 @@
+const myName = "Christopher";
+let myAge = 25;
+let isStudent = true;
+
+console.log("My name is " + myName);
+console.log("I am " + myAge + " years old.");
+console.log("Am I a student? " + isStudent);    
