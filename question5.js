00d26ad8cd07b4c449c6e57e6abdfc8914ca68c7
kemi Chris <ekeminichristopher123@gmail.com>
2026-04-18 11:23:19 +0100

    question 5

---
+++ question5.js
@@ -0,0 +1,8 @@
+// Use the map() method on the numbers array below to create a NEW array where every number is doubled. Log the new array. Do NOT change the original.
+
+const numbers = [3, 6, 9, 12, 15];
+
+const doubleNumbers = numbers.map(num => num * 2);
+
+console.log(doubleNumbers);
+console.log(numbers);
