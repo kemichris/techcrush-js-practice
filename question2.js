ef1530ccd9431379e858636c67dd3b53f23d2085
kemi Chris <ekeminichristopher123@gmail.com>
2026-04-18 11:16:26 +0100

    question 2

---
+++ question2.js
@@ -0,0 +1,6 @@
+// Write an arrow function called multiply that takes two numbers and returns their product. Then call it with 6 and 7 and log the result.
+
+const multiply = (a, b) =>  a * b;
+
+const result = multiply(6, 7);
+console.log(result);
