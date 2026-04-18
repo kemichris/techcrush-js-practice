ad5b30c3d3d5407eedb678a6c28bc445a00ad5bf
kemi Chris <ekeminichristopher123@gmail.com>
2026-04-18 11:20:25 +0100

    question 4

---
+++ question4.js
@@ -0,0 +1,12 @@
+// Given the array below, write code to:
+// 1. Add 'pineapple' to the end
+// 2. Remove the first item
+// 3. Log the updated array and its length
+
+const fruits = ["mango", "orange", "banana"];
+
+fruits.push("pineapple");
+fruits.shift();
+
+console.log(fruits);
+console.log(fruits.length);
